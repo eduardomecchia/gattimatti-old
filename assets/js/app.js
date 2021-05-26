d66b17ebac7fbfa3159114e5bf89139711125287
Eduardo Mecchia <eduardomecchia273@gmail.com>
2021-05-26 12:47:49 +0200

    Add pseudo-random images on page load and buttons to change them

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,45 @@
 const app = new Vue({
     el: "#app",
 
-    data: {},
+    data: {
+        currentWhiskeyPic: null,
 
-    methods: {},
+        whiskeyPics: [
+            "prisk_01",
+            "prisk_02",
+            "prisk_03"
+        ],
 
-    mounted() {}
+        currentZeldaPic: null,
+
+        zeldaPics: [
+            "zelda_01",
+            "zelda_02",
+            "zelda_03"
+        ]
+    },
+
+    computed: {
+        
+    },
+
+    methods: {
+        /**
+         * Update the pic of the given array
+         * @param {array} array 
+         */
+        getRandomPhoto(array) {
+            const randomNumber = Math.floor(Math.random() * array.length);
+
+            this.currentPic = array[randomNumber];
+        }
+    },
+
+    created() {
+        const randomWhiskey = Math.floor(Math.random() * this.whiskeyPics.length);
+        const randomZelda = Math.floor(Math.random() * this.zeldaPics.length);
+
+        this.currentWhiskeyPic = this.whiskeyPics[randomWhiskey];
+        this.currentZeldaPic = this.zeldaPics[randomZelda];
+    }
 });
